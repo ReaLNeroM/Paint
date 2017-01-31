67f23891e0a07f9b2e473c78d7b84200b922c779
Vladimir <vlade.maksimovski@gmail.com>
2017-01-31 23:55:56 +0100

    Added the files

---
+++ drawing.js
@@ -0,0 +1,26 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var cursorX = -1;
+var cursorY = -1;
+var mouse_clicked = false;
+
+document.onmousedown = function(){
+	mouse_clicked = true;
+}
+document.onmouseup = function(){
+	mouse_clicked = false;
+}
+document.onmousemove = function(e){
+	if(mouse_clicked && cursorX != -1 && cursorY != -1){
+		ctx.beginPath();
+		ctx.moveTo(cursorX, cursorY);
+		ctx.lineTo(e.pageX, e.pageY);
+		ctx.stroke();
+	}
+    cursorX = e.pageX;
+    cursorY = e.pageY;
+};
