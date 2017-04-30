7db3d737b8cce65aebe94d50dd5429b8e9bd7a55
Vladimir M <vlade.maksimovski@gmail.com>
2017-05-01 00:42:19 +0300

    Refactoring

--- drawing.js
+++ drawing.js
@@ -11,9 +11,11 @@ var mouse_clicked = false;
 document.onmousedown = function(){
 	mouse_clicked = true;
 }
+
 document.onmouseup = function(){
 	mouse_clicked = false;
 }
+
 document.onmousemove = function(e){
 	if(mouse_clicked && cursorX != -1 && cursorY != -1){
 		ctx.beginPath();
